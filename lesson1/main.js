8f6c2bb6a9422baa81b1fb64f97bf240e5682d13
ArmenArutiunian <armenasata@gmail.com>
2023-01-12 21:27:45 +0100

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,60 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let n = 3.14;
+let y = 2.7;
+let q = 16;
+let w = true;
+let x = false;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(n);
+console.log(y);
+console.log(q);
+console.log(w);
+console.log(x);
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person
+// (Не об'єкт, просто за допомоги конкатенації)
+
+
+let firstName = 'Arutiunian' ;
+let middleName = 'Armen' ;
+let lastName = 'Aramovich' ;
+
+console.log(firstName + ' ' + middleName + ' ' + lastName);
+
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+let aa = 100;
+let bb = '100';
+let cc = true;
+
+console.log(typeof aa,bb,cc);
+
+
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let name2 = prompt('Please Name');
+let lastName2 = prompt('Please lastName');
+let year2 = prompt('Please year');
+
+console.log(name2)
+console.log(lastName2)
+console.log(year2)
